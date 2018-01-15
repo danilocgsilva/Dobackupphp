f173537b3e89bc67d1ef0627c21c2836b9688439
Danilo Silva <danilocgsilva@gmail.com>
2018-01-15 17:30:33 -0200

    Gitignore updated

--- web/api/default.php
+++ web/api/default.php
@@ -4,9 +4,12 @@ require '../vendor/autoload.php';
 
 $valuesArray = $secure->hostValues();
 
+$folder_to_write = getcwd() . DIRECTORY_SEPARATOR . 'backupfiles';
+
 \Danilocgsilva\Backupphp::backup(
     $valuesArray['host'],
     $valuesArray['user'],
     $valuesArray['dbname'],
-    $valuesArray['pass']
+    $valuesArray['pass'],
+    $folder_to_write
 );
